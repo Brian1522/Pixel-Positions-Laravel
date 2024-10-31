e400e4566bdfea416ce7a0dbd9746a71553bdd31
Brian Laguna <66448870+Brian1522@users.noreply.github.com>
2024-10-31 15:26:47 -0600

    Episode28_Blade_and_Tailwind_Techniques_for_Your_Laravel_Views

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,19 +1,17 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+<x-panel class="flex gap-x-6">
     <div>
-        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+        <x-employer-logo />
     </div>
-    <div class="self-start text-sm">Laracasts
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">Laracasts</a>
 
-        <h3>Video Producer</h3>
-        <p>Full Time -From $60,000</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">Video Producer</h3>
+        
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
     </div>
-
     <div>
         <x-tag>Tag</x-tag>
         <x-tag>Tag</x-tag>
         <x-tag>Tag</x-tag>
     </div>
-
-
-
-</div>
+</x-panel>
